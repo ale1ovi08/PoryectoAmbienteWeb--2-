0c7b9e23bce79979724b9d4ec826b746aff07303
Fabian-AM <fabian.alonso238@gmail.com>
2025-03-29 20:54:44 -0600

    Correccion git

--- README.md
+++
@@ -1,1 +0,0 @@
-"# PoryectoAmbienteWeb--2-" 
--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php   
-$conexion= new mysqli("localhost:3308","admin","12345",'ProyectoAgricultura');
-
-if($conexion->connect_error){
-    die("Conexion fallida: " . $conexion->connect_error);
-}
-?>
